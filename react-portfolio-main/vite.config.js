2c7ac74b0136d6d1b51196959fb08c87a979805a
prajakta2809 <prajaktadigraje13@gmail.com>
2024-09-15 15:12:33 +0530

    deployment code

--- react-portfolio-main/vite.config.js
+++ react-portfolio-main/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/react-portfolio-main/",
+  base:"/react-portfolio-main/react-portfolio-main/",
   css: {
     modules: {
       localsConvention: "camelCase",
